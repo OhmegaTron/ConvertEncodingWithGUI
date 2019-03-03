62ffbee10a42bd0a64498cc6eeed62dde3b9f6c9
ohm88 <ohm88@OHM-ITX>
2019-03-03 16:46:56 +0100

    Fixed comboboxes

--- FirstTry_VBForm/Form1.Designer.vb
+++ FirstTry_VBForm/Form1.Designer.vb
@@ -22,10 +22,263 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.lblDescription = New System.Windows.Forms.Label()
+        Me.txtBoxEncodingOutput = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.cmbBoxOutputEncoding = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtBoxFilePathOutput = New System.Windows.Forms.TextBox()
+        Me.btnCloseProgram = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtBxEncodingInput = New System.Windows.Forms.TextBox()
+        Me.txtBoxFilePathInput = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.cmbBoxInputEncoding = New System.Windows.Forms.ComboBox()
+        Me.txtBoxTest = New System.Windows.Forms.TextBox()
+        Me.btnTest = New System.Windows.Forms.Button()
+        Me.Panel2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(675, 261)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 0
+        Me.btnConvert.Text = "&Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'lblDescription
+        '
+        Me.lblDescription.AutoSize = True
+        Me.lblDescription.Location = New System.Drawing.Point(12, 46)
+        Me.lblDescription.Name = "lblDescription"
+        Me.lblDescription.Size = New System.Drawing.Size(126, 13)
+        Me.lblDescription.TabIndex = 1
+        Me.lblDescription.Text = "Desription of this program"
+        '
+        'txtBoxEncodingOutput
+        '
+        Me.txtBoxEncodingOutput.Location = New System.Drawing.Point(79, 70)
+        Me.txtBoxEncodingOutput.Name = "txtBoxEncodingOutput"
+        Me.txtBoxEncodingOutput.Size = New System.Drawing.Size(105, 20)
+        Me.txtBoxEncodingOutput.TabIndex = 13
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 12)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(123, 13)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Output file and encoding"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.cmbBoxOutputEncoding)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Controls.Add(Me.txtBoxFilePathOutput)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Controls.Add(Me.txtBoxEncodingOutput)
+        Me.Panel2.Location = New System.Drawing.Point(446, 90)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(400, 150)
+        Me.Panel2.TabIndex = 17
+        '
+        'cmbBoxOutputEncoding
+        '
+        Me.cmbBoxOutputEncoding.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbBoxOutputEncoding.FormattingEnabled = True
+        Me.cmbBoxOutputEncoding.Location = New System.Drawing.Point(24, 112)
+        Me.cmbBoxOutputEncoding.Name = "cmbBoxOutputEncoding"
+        Me.cmbBoxOutputEncoding.Size = New System.Drawing.Size(361, 21)
+        Me.cmbBoxOutputEncoding.TabIndex = 19
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(21, 73)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 13)
+        Me.Label5.TabIndex = 18
+        Me.Label5.Text = "Encoding"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(34, 46)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 13)
+        Me.Label4.TabIndex = 17
+        Me.Label4.Text = "FilePath"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(310, 38)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 16
+        Me.Button2.Text = "Browse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'txtBoxFilePathOutput
+        '
+        Me.txtBoxFilePathOutput.Location = New System.Drawing.Point(79, 40)
+        Me.txtBoxFilePathOutput.Name = "txtBoxFilePathOutput"
+        Me.txtBoxFilePathOutput.Size = New System.Drawing.Size(225, 20)
+        Me.txtBoxFilePathOutput.TabIndex = 14
+        '
+        'btnCloseProgram
+        '
+        Me.btnCloseProgram.Location = New System.Drawing.Point(774, 261)
+        Me.btnCloseProgram.Name = "btnCloseProgram"
+        Me.btnCloseProgram.Size = New System.Drawing.Size(75, 23)
+        Me.btnCloseProgram.TabIndex = 18
+        Me.btnCloseProgram.Text = "E&xit"
+        Me.btnCloseProgram.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Input file and encoding"
+        '
+        'txtBxEncodingInput
+        '
+        Me.txtBxEncodingInput.Location = New System.Drawing.Point(68, 73)
+        Me.txtBxEncodingInput.Name = "txtBxEncodingInput"
+        Me.txtBxEncodingInput.Size = New System.Drawing.Size(121, 20)
+        Me.txtBxEncodingInput.TabIndex = 2
+        '
+        'txtBoxFilePathInput
+        '
+        Me.txtBoxFilePathInput.Location = New System.Drawing.Point(68, 47)
+        Me.txtBoxFilePathInput.Name = "txtBoxFilePathInput"
+        Me.txtBoxFilePathInput.Size = New System.Drawing.Size(238, 20)
+        Me.txtBoxFilePathInput.TabIndex = 12
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 50)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "FilePath"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(312, 44)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "Browse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(10, 76)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(52, 13)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Encoding"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.cmbBoxInputEncoding)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.txtBxEncodingInput)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.txtBoxFilePathInput)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Location = New System.Drawing.Point(3, 90)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(400, 150)
+        Me.Panel1.TabIndex = 16
+        '
+        'cmbBoxInputEncoding
+        '
+        Me.cmbBoxInputEncoding.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.cmbBoxInputEncoding.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbBoxInputEncoding.FormattingEnabled = True
+        Me.cmbBoxInputEncoding.Location = New System.Drawing.Point(9, 112)
+        Me.cmbBoxInputEncoding.Name = "cmbBoxInputEncoding"
+        Me.cmbBoxInputEncoding.Size = New System.Drawing.Size(364, 21)
+        Me.cmbBoxInputEncoding.TabIndex = 16
+        '
+        'txtBoxTest
+        '
+        Me.txtBoxTest.Location = New System.Drawing.Point(26, 261)
+        Me.txtBoxTest.Name = "txtBoxTest"
+        Me.txtBoxTest.Size = New System.Drawing.Size(100, 20)
+        Me.txtBoxTest.TabIndex = 19
+        '
+        'btnTest
+        '
+        Me.btnTest.Location = New System.Drawing.Point(148, 261)
+        Me.btnTest.Name = "btnTest"
+        Me.btnTest.Size = New System.Drawing.Size(75, 23)
+        Me.btnTest.TabIndex = 20
+        Me.btnTest.Text = "Button3"
+        Me.btnTest.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(858, 306)
+        Me.Controls.Add(Me.btnTest)
+        Me.Controls.Add(Me.txtBoxTest)
+        Me.Controls.Add(Me.btnCloseProgram)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lblDescription)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.Panel2)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents lblDescription As Label
+    Friend WithEvents txtBoxEncodingOutput As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents txtBoxFilePathOutput As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents btnCloseProgram As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtBxEncodingInput As TextBox
+    Friend WithEvents txtBoxFilePathInput As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents cmbBoxInputEncoding As ComboBox
+    Friend WithEvents cmbBoxOutputEncoding As ComboBox
+    Friend WithEvents txtBoxTest As TextBox
+    Friend WithEvents btnTest As Button
 End Class
--- FirstTry_VBForm/Form1.vb
+++ FirstTry_VBForm/Form1.vb
@@ -1,3 +1,254 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    Public Function EncodingList() As DataTable
+        Dim encodingTable As New DataTable()
+        encodingTable.Columns.Add("id")
+        encodingTable.Columns.Add("name")
+        encodingTable.Columns.Add("description")
+
+        encodingTable.Rows.Add("37", "IBM037", "IBM EBCDIC (US-Canada)")
+        encodingTable.Rows.Add("437", "IBM437", "OEM United States")
+        encodingTable.Rows.Add("500", "IBM500", "IBM EBCDIC (International)")
+        encodingTable.Rows.Add("708", "ASMO-708", "Arabic (ASMO 708)")
+        encodingTable.Rows.Add("720", "DOS-720", "Arabic (DOS)")
+        encodingTable.Rows.Add("737", "ibm737", "Greek (DOS)")
+        encodingTable.Rows.Add("775", "ibm775", "Baltic (DOS)")
+        encodingTable.Rows.Add("850", "ibm850", "Western European (DOS)")
+        encodingTable.Rows.Add("852", "ibm852", "Central European (DOS)")
+        encodingTable.Rows.Add("855", "IBM855", "OEM Cyrillic")
+        encodingTable.Rows.Add("857", "ibm857", "Turkish (DOS)")
+        encodingTable.Rows.Add("858", "IBM00858", "OEM Multilingual Latin I")
+        encodingTable.Rows.Add("860", "IBM860", "Portuguese (DOS)")
+        encodingTable.Rows.Add("861", "ibm861", "Icelandic (DOS)")
+        encodingTable.Rows.Add("862", "DOS-862", "Hebrew (DOS)")
+        encodingTable.Rows.Add("863", "IBM863", "French Canadian (DOS)")
+        encodingTable.Rows.Add("864", "IBM864", "Arabic (864)")
+        encodingTable.Rows.Add("865", "IBM865", "Nordic (DOS)")
+        encodingTable.Rows.Add("866", "cp866", "Cyrillic (DOS)")
+        encodingTable.Rows.Add("869", "ibm869", "Greek, Modern (DOS)")
+        encodingTable.Rows.Add("870", "IBM870", "IBM EBCDIC (Multilingual Latin-2)")
+        encodingTable.Rows.Add("874", "windows-874", "Thai (Windows)")
+        encodingTable.Rows.Add("875", "cp875", "IBM EBCDIC (Greek Modern)")
+        encodingTable.Rows.Add("932", "shift_jis", "Japanese (Shift-JIS)")
+        encodingTable.Rows.Add("936", "gb2312", "Chinese Simplified (GB2312)")
+        encodingTable.Rows.Add("949", "ks_c_5601-1987", "Korean")
+        encodingTable.Rows.Add("950", "big5", "Chinese Traditional (Big5)")
+        encodingTable.Rows.Add("1026", "IBM1026", "IBM EBCDIC (Turkish Latin-5)")
+        encodingTable.Rows.Add("1047", "IBM01047", "IBM Latin-1")
+        encodingTable.Rows.Add("1140", "IBM01140", "IBM EBCDIC (US-Canada-Euro)")
+        encodingTable.Rows.Add("1141", "IBM01141", "IBM EBCDIC (Germany-Euro)")
+        encodingTable.Rows.Add("1142", "IBM01142", "IBM EBCDIC (Denmark-Norway-Euro)")
+        encodingTable.Rows.Add("1143", "IBM01143", "IBM EBCDIC (Finland-Sweden-Euro)")
+        encodingTable.Rows.Add("1144", "IBM01144", "IBM EBCDIC (Italy-Euro)")
+        encodingTable.Rows.Add("1145", "IBM01145", "IBM EBCDIC (Spain-Euro)")
+        encodingTable.Rows.Add("1146", "IBM01146", "IBM EBCDIC (UK-Euro)")
+        encodingTable.Rows.Add("1147", "IBM01147", "IBM EBCDIC (France-Euro)")
+        encodingTable.Rows.Add("1148", "IBM01148", "IBM EBCDIC (International-Euro)")
+        encodingTable.Rows.Add("1149", "IBM01149", "IBM EBCDIC (Icelandic-Euro)")
+        encodingTable.Rows.Add("1200", "utf-16", "Unicode")
+        encodingTable.Rows.Add("1201", "unicodeFFFE", "Unicode (Big endian)")
+        encodingTable.Rows.Add("1250", "windows-1250", "Central European (Windows)")
+        encodingTable.Rows.Add("1251", "windows-1251", "Cyrillic (Windows)")
+        encodingTable.Rows.Add("1252", "Windows-1252", "Western European (Windows)")
+        encodingTable.Rows.Add("1253", "windows-1253", "Greek (Windows)")
+        encodingTable.Rows.Add("1254", "windows-1254", "Turkish (Windows)")
+        encodingTable.Rows.Add("1255", "windows-1255", "Hebrew (Windows)")
+        encodingTable.Rows.Add("1256", "windows-1256", "Arabic (Windows)")
+        encodingTable.Rows.Add("1257", "windows-1257", "Baltic (Windows)")
+        encodingTable.Rows.Add("1258", "windows-1258", "Vietnamese (Windows)")
+        encodingTable.Rows.Add("1361", "Johab", "Korean (Johab)")
+        encodingTable.Rows.Add("10000", "macintosh", "Western European (Mac)")
+        encodingTable.Rows.Add("10001", "x-mac-japanese", "Japanese (Mac)")
+        encodingTable.Rows.Add("10002", "x-mac-chinesetrad", "Chinese Traditional (Mac)")
+        encodingTable.Rows.Add("10003", "x-mac-korean", "Korean (Mac)")
+        encodingTable.Rows.Add("10004", "x-mac-arabic", "Arabic (Mac)")
+        encodingTable.Rows.Add("10005", "x-mac-hebrew", "Hebrew (Mac)")
+        encodingTable.Rows.Add("10006", "x-mac-greek", "Greek (Mac)")
+        encodingTable.Rows.Add("10007", "x-mac-cyrillic", "Cyrillic (Mac)")
+        encodingTable.Rows.Add("10008", "x-mac-chinesesimp", "Chinese Simplified (Mac)")
+        encodingTable.Rows.Add("10010", "x-mac-romanian", "Romanian (Mac)")
+        encodingTable.Rows.Add("10017", "x-mac-ukrainian", "Ukrainian (Mac)")
+        encodingTable.Rows.Add("10021", "x-mac-thai", "Thai (Mac)")
+        encodingTable.Rows.Add("10029", "x-mac-ce", "Central European (Mac)")
+        encodingTable.Rows.Add("10079", "x-mac-icelandic", "Icelandic (Mac)")
+        encodingTable.Rows.Add("10081", "x-mac-turkish", "Turkish (Mac)")
+        encodingTable.Rows.Add("10082", "x-mac-croatian", "Croatian (Mac)")
+        encodingTable.Rows.Add("12000", "utf-32", "Unicode (UTF-32)")
+        encodingTable.Rows.Add("12001", "utf-32BE", "Unicode (UTF-32 Big endian)")
+        encodingTable.Rows.Add("20000", "x-Chinese-CNS", "Chinese Traditional (CNS)")
+        encodingTable.Rows.Add("20001", "x-cp20001", "TCA Taiwan")
+        encodingTable.Rows.Add("20002", "x-Chinese-Eten", "Chinese Traditional (Eten)")
+        encodingTable.Rows.Add("20003", "x-cp20003", "IBM5550 Taiwan")
+        encodingTable.Rows.Add("20004", "x-cp20004", "TeleText Taiwan")
+        encodingTable.Rows.Add("20005", "x-cp20005", "Wang Taiwan")
+        encodingTable.Rows.Add("20105", "x-IA5", "Western European (IA5)")
+        encodingTable.Rows.Add("20106", "x-IA5-German", "German (IA5)")
+        encodingTable.Rows.Add("20107", "x-IA5-Swedish", "Swedish (IA5)")
+        encodingTable.Rows.Add("20108", "x-IA5-Norwegian", "Norwegian (IA5)")
+        encodingTable.Rows.Add("20127", "us-ascii", "US-ASCII")
+        encodingTable.Rows.Add("20261", "x-cp20261", "T.61")
+        encodingTable.Rows.Add("20269", "x-cp20269", "ISO-6937")
+        encodingTable.Rows.Add("20273", "IBM273", "IBM EBCDIC (Germany)")
+        encodingTable.Rows.Add("20277", "IBM277", "IBM EBCDIC (Denmark-Norway)")
+        encodingTable.Rows.Add("20278", "IBM278", "IBM EBCDIC (Finland-Sweden)")
+        encodingTable.Rows.Add("20280", "IBM280", "IBM EBCDIC (Italy)")
+        encodingTable.Rows.Add("20284", "IBM284", "IBM EBCDIC (Spain)")
+        encodingTable.Rows.Add("20285", "IBM285", "IBM EBCDIC (UK)")
+        encodingTable.Rows.Add("20290", "IBM290", "IBM EBCDIC (Japanese katakana)")
+        encodingTable.Rows.Add("20297", "IBM297", "IBM EBCDIC (France)")
+        encodingTable.Rows.Add("20420", "IBM420", "IBM EBCDIC (Arabic)")
+        encodingTable.Rows.Add("20423", "IBM423", "IBM EBCDIC (Greek)")
+        encodingTable.Rows.Add("20424", "IBM424", "IBM EBCDIC (Hebrew)")
+        encodingTable.Rows.Add("20833", "x-EBCDIC-KoreanExtended", "IBM EBCDIC (Korean Extended)")
+        encodingTable.Rows.Add("20838", "IBM-Thai", "IBM EBCDIC (Thai)")
+        encodingTable.Rows.Add("20866", "koi8-r", "Cyrillic (KOI8-R)")
+        encodingTable.Rows.Add("20871", "IBM871", "IBM EBCDIC (Icelandic)")
+        encodingTable.Rows.Add("20880", "IBM880", "IBM EBCDIC (Cyrillic Russian)")
+        encodingTable.Rows.Add("20905", "IBM905", "IBM EBCDIC (Turkish)")
+        encodingTable.Rows.Add("20924", "IBM00924", "IBM Latin-1")
+        encodingTable.Rows.Add("20932", "EUC-JP", "Japanese (JIS 0208-1990 and 0212-1990)")
+        encodingTable.Rows.Add("20936", "x-cp20936", "Chinese Simplified (GB2312-80)")
+        encodingTable.Rows.Add("20949", "x-cp20949", "Korean Wansung")
+        encodingTable.Rows.Add("21025", "cp1025", "IBM EBCDIC (Cyrillic Serbian-Bulgarian)")
+        encodingTable.Rows.Add("21866", "koi8-u", "Cyrillic (KOI8-U)")
+        encodingTable.Rows.Add("28591", "iso-8859-1", "Western European (ISO)")
+        encodingTable.Rows.Add("28592", "iso-8859-2", "Central European (ISO)")
+        encodingTable.Rows.Add("28593", "iso-8859-3", "Latin 3 (ISO)")
+        encodingTable.Rows.Add("28594", "iso-8859-4", "Baltic (ISO)")
+        encodingTable.Rows.Add("28595", "iso-8859-5", "Cyrillic (ISO)")
+        encodingTable.Rows.Add("28596", "iso-8859-6", "Arabic (ISO)")
+        encodingTable.Rows.Add("28597", "iso-8859-7", "Greek (ISO)")
+        encodingTable.Rows.Add("28598", "iso-8859-8", "Hebrew (ISO-Visual)")
+        encodingTable.Rows.Add("28599", "iso-8859-9", "Turkish (ISO)")
+        encodingTable.Rows.Add("28603", "iso-8859-13", "Estonian (ISO)")
+        encodingTable.Rows.Add("28605", "iso-8859-15", "Latin 9 (ISO)")
+        encodingTable.Rows.Add("29001", "x-Europa", "Europa")
+        encodingTable.Rows.Add("38598", "iso-8859-8-i", "Hebrew (ISO-Logical)")
+        encodingTable.Rows.Add("50220", "iso-2022-jp", "Japanese (JIS)")
+        encodingTable.Rows.Add("50221", "csISO2022JP", "Japanese (JIS-Allow 1 byte Kana)")
+        encodingTable.Rows.Add("50222", "iso-2022-jp", "Japanese (JIS-Allow 1 byte Kana - SO/SI)")
+        encodingTable.Rows.Add("50225", "iso-2022-kr", "Korean (ISO)")
+        encodingTable.Rows.Add("50227", "x-cp50227", "Chinese Simplified (ISO-2022)")
+        encodingTable.Rows.Add("51932", "euc-jp", "Japanese (EUC)")
+        encodingTable.Rows.Add("51936", "EUC-CN", "Chinese Simplified (EUC)")
+        encodingTable.Rows.Add("51949", "euc-kr", "Korean (EUC)")
+        encodingTable.Rows.Add("52936", "hz-gb-2312", "Chinese Simplified (HZ)")
+        encodingTable.Rows.Add("54936", "GB18030", "Chinese Simplified (GB18030)")
+        encodingTable.Rows.Add("57002", "x-iscii-de", "ISCII Devanagari")
+        encodingTable.Rows.Add("57003", "x-iscii-be", "ISCII Bengali")
+        encodingTable.Rows.Add("57004", "x-iscii-ta", "ISCII Tamil")
+        encodingTable.Rows.Add("57005", "x-iscii-te", "ISCII Telugu")
+        encodingTable.Rows.Add("57006", "x-iscii-as", "ISCII Assamese")
+        encodingTable.Rows.Add("57007", "x-iscii-or", "ISCII Oriya")
+        encodingTable.Rows.Add("57008", "x-iscii-ka", "ISCII Kannada")
+        encodingTable.Rows.Add("57009", "x-iscii-ma", "ISCII Malayalam")
+        encodingTable.Rows.Add("57010", "x-iscii-gu", "ISCII Gujarati")
+        encodingTable.Rows.Add("57011", "x-iscii-pa", "ISCII Punjabi")
+        encodingTable.Rows.Add("65000", "utf-7", "Unicode (UTF-7)")
+        encodingTable.Rows.Add("65001", "utf-8", "Unicode (UTF-8)")
+
+        EncodingList = encodingTable
+    End Function
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+
+        ' Endcodings https://docs.microsoft.com/en-us/dotnet/api/system.text.encoding?view=netframework-4.7.2
+        'Private Sub TranslateFile(ByVal sourceEbcdicFilePath As String, ByVal newAsciiFilePath As String)
+        ' System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance)
+
+        'Get encoding objects from values in comboboxes
+        Dim inputEncodingID As String = cmbBoxInputEncoding.SelectedValue("id").ToString
+        Dim outputEncodingID As String = cmbBoxOutputEncoding.SelectedValue("id").ToString
+        Dim inputEncoding As System.Text.Encoding = System.Text.Encoding.GetEncoding(inputEncodingID)
+        Dim outputEncoding As System.Text.Encoding = System.Text.Encoding.GetEncoding(outputEncodingID)
+
+
+        'Set the encoding to the EBCDIC code page.
+        'Dim encoding As System.Text.Encoding = System.Text.Encoding.GetEncoding(37)
+
+        'Set this to the length of an EBCDIC line or block.
+        Dim lineLength As Integer = 134
+
+        'Buffer used to store characters read in from the statement input file.
+        Dim buffer(lineLength - 1) As Char
+
+        'Open the EBCDIC file for reading using the EBCDIC encoding.
+        Dim reader As New IO.StreamReader(txtBoxFilePathInput.Text, inputEncoding)
+
+        'Open a file to write out the data to.
+        Dim writer As New IO.StreamWriter(txtBoxFilePathOutput.Text, False, outputEncoding)
+
+        'This is a string to store the translated line.
+        Dim strAscii As String = String.Empty
+
+        'This variable increments every time a block of data is read
+        Dim iLoops As Integer = 0
+
+        'Loop through the EBCDIC file.
+        Do Until reader.EndOfStream = True
+
+            'Read in a block of data from the EBCDIC file.
+            reader.ReadBlock(buffer, 0, lineLength)
+
+            'Translate the string using the EBCDIC encoding.
+            strAscii = inputEncoding.GetString(inputEncoding.GetBytes(buffer))
+
+            'Write the translated string out to a file.
+            writer.WriteLine(strAscii)
+
+            'Only call DoEvents every 1000 loops (increases performance)
+            ' iLoops += 1
+            ' If iLoops = 1000 Then
+
+            '  Application.DoEvents()
+
+            '  iLoops = 0 'reset
+
+            ' End If
+
+        Loop
+
+        'Close files.
+        reader.Close()
+        writer.Close()
+
+        'Release resources.
+        reader.Dispose()
+        writer.Dispose()
+
+    End Sub
+
+    Private Sub txtBoxFilePathInput_TextChanged(sender As Object, e As EventArgs) Handles txtBoxFilePathInput.TextChanged
+
+    End Sub
+
+    Private Sub CloseProgram(sender As Object, e As EventArgs) Handles btnCloseProgram.Click
+        'Exit button, closes the program
+        Me.Close()
+    End Sub
+
+    Private Sub comboBoxLoad(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Populate input encoding combobox
+        cmbBoxInputEncoding.DataSource = EncodingList()
+        cmbBoxInputEncoding.DisplayMember = "description"
+    End Sub
+
+    Private Sub cmbBoxOutputEncoding_SelectedIndexChanged(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Populate output encoding combobox
+        cmbBoxOutputEncoding.DataSource = EncodingList()
+        cmbBoxOutputEncoding.DisplayMember = "description"
+    End Sub
+
+    Private Sub cmbBoxInputEncoding_SelectedIndexChanged_1(sender As Object, e As EventArgs) Handles cmbBoxInputEncoding.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub cmbBoxOutputEncoding_SelectedIndexChanged_1(sender As Object, e As EventArgs) Handles cmbBoxOutputEncoding.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+        txtBoxTest.Text = cmbBoxInputEncoding.SelectedValue("id").ToString
+    End Sub
 End Class
